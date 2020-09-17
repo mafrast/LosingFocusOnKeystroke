dbcc2a2bfbf9901e6974e8412e3ea01a0df5a94d
Mathias Stepper <mathias.stepper@brz.eu>
2020-09-17 15:22:17 +0200

    Add project files.

---
+++ LosingFocusOnKeystroke/Data/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LosingFocusOnKeystroke.Data
+{
+    public class ViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string BusinessKey { get; set; }
+
+        public string Name { get; set; }
+
+        public ViewModel Parent { get; set; }
+
+        public Guid? ParentId => Parent?.Id;
+    }
+}
